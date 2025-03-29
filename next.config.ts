f408ac6d43d76611bf82c8117d4d53cdcc38b6ce
ncg <Vasu Grover>
2025-03-30 01:13:23 +0530

    feat: Dockerfile for hosting

--- next.config.ts
+++ next.config.ts
@@ -1,62 +1,65 @@
 import type { NextConfig } from "next";
 import withPWA from 'next-pwa';
 
-const nextConfig: NextConfig = withPWA({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
-  sw: 'sw.js',
-  runtimeCaching: [
-    {
-      urlPattern: /^https?.*/,
-      handler: 'NetworkFirst',
-      options: {
-        cacheName: 'offlineCache',
-        expiration: {
-          maxEntries: 200
+const nextConfig: NextConfig = {
+  output: 'standalone',
+  ...withPWA({
+    dest: 'public',
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === 'development',
+    sw: 'sw.js',
+    runtimeCaching: [
+      {
+        urlPattern: /^https?.*/,
+        handler: 'NetworkFirst',
+        options: {
+          cacheName: 'offlineCache',
+          expiration: {
+            maxEntries: 200
+          }
+        }
+      },
+      {
+        urlPattern: /\/_next\/static\/.*/,
+        handler: 'CacheFirst',
+        options: {
+          cacheName: 'static-assets',
+          expiration: {
+            maxEntries: 100,
+            maxAgeSeconds: 60 * 60 * 24 * 30 // 30 days
+          }
+        }
+      },
+      {
+        urlPattern: /\/_next\/app-build-manifest\.json$/,
+        handler: 'NetworkOnly'
+      },
+      {
+        urlPattern: /\/_next\/static\/chunks\/webpack\.js$/,
+        handler: 'NetworkOnly'
+      },
+      {
+        urlPattern: /\/_next\/static\/development\/_buildManifest\.js$/,
+        handler: 'NetworkOnly'
+      },
+      {
+        urlPattern: /\/_next\/static\/development\/_ssgManifest\.js$/,
+        handler: 'NetworkOnly'
+      },
+      {
+        urlPattern: /\/.*/,
+        handler: 'StaleWhileRevalidate',
+        options: {
+          cacheName: 'pages',
+          expiration: {
+            maxEntries: 50,
+            maxAgeSeconds: 60 * 60 * 24 // 1 day
+          }
         }
       }
-    },
-    {
-      urlPattern: /\/_next\/static\/.*/,
-      handler: 'CacheFirst',
-      options: {
-        cacheName: 'static-assets',
-        expiration: {
-          maxEntries: 100,
-          maxAgeSeconds: 60 * 60 * 24 * 30 // 30 days
-        }
-      }
-    },
-    {
-      urlPattern: /\/_next\/app-build-manifest\.json$/,
-      handler: 'NetworkOnly'
-    },
-    {
-      urlPattern: /\/_next\/static\/chunks\/webpack\.js$/,
-      handler: 'NetworkOnly'
-    },
-    {
-      urlPattern: /\/_next\/static\/development\/_buildManifest\.js$/,
-      handler: 'NetworkOnly'
-    },
-    {
-      urlPattern: /\/_next\/static\/development\/_ssgManifest\.js$/,
-      handler: 'NetworkOnly'
-    },
-    {
-      urlPattern: /\/.*/,
-      handler: 'StaleWhileRevalidate',
-      options: {
-        cacheName: 'pages',
-        expiration: {
-          maxEntries: 50,
-          maxAgeSeconds: 60 * 60 * 24 // 1 day
-        }
-      }
-    }
-  ]
-});
+    ]
+  })
+};
 
 export default nextConfig;
